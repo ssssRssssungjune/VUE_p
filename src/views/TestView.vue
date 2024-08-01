b7e2e0de2a9458dd037cc4f504cd7921fbb0ca31
ssssRssssungjune <ssungjune0610@gmail.com>
2024-08-01 17:27:23 +0900

    이름 한거

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,127 +1,55 @@
 <template>
+  <br><br><br><br><br><br>
   <div class="">
-    <select name="" id="">
-      <option value="서울">서울</option>
-      <option value="부산">부산</option>
-      <option value="대구">대구</option>
-      <option value="수원">수원</option>
-      <option value="광주" selected>광주</option>
-    </select>
-    <select v-model="city" @change="changeCity">
-      <option value="서울">서울</option>
-      <option value="부산">부산</option>
-      <option value="대전">대전</option>
-      <option value="수원">수원</option>
-      <option value="광주">광주</option>
-    </select>
-    <p>{{ message }}</p>
-  </div>
-  <div>
-    <p>
-      <label
-        >이메일<input
-          type="text"
-          placeholder="이메일을 입력하세요"
-          v-model="emailValue"
-          @input="changeEmail" />
-          <p v-if="msgEmail">{{ errPwd }}</p>
-          </label
-      ><span>{{ msgEmail }}</span>
-      
-    </p>
-    <p>
-      <label
-        >비밀번호<input
-          type="text"
-          v-model="pwdValue1"
-          @input="changePwd1"
-          placeholder="비밀번호를 입력하세요"
-      /></label>
-      <span>{{ errPwd  }}</span>
-    </p>
-    <p>
-      <label
-        >비밀번호확인<input
-          type="text"
-          v-model="pwdValue2"
-          @input="changePwd2"
-          placeholder="비밀번호확인을 입력하세요"
-      /></label>
-      
-    </p>
+    <label > 성 : <input type="text" v-model="familyName1" @input="makeFullName1"></label>
+    <label > 이름: <input type="text" v-model="name1" @input="makeFullName1"> </label>
+    <p>성명(method) : {{ fullName1 }}</p>
+  </div><br><br>
+  <div class="">
+    <label > 성 : <input type="text" v-model="familyName2"></label>
+    <label > 이름: <input type="text" v-model="name2"> </label>
+    <p>성명(computed) : {{ makeFullName2 }}</p>
+  </div><br><br>
+  <div class="">
+    <label > 성 : <input type="text" v-model="familyName3"></label>
+    <label > 이름: <input type="text" v-model="name3"> </label>
+    <p>성명(watch) : {{ fullName3 }}</p>
   </div>
 </template>
 <script>
 export default {
-  name: "TestView",
+  name: 'TestView',
   data() {
     return {
-      city: "서울",
-      message: "안녕하세요 반갑습니다.",
-      emailValue: "",
-      msgEmail: "",
-      errPwd: "",
-      errEmail: "",
-      pwdValue1: "",
-      pwdValue2: "",
+        familyName1:'',
+        name1:'',
+        fullName1:'',
+        familyName2:'',
+        name2:'',
+        familyName3:'',
+        name3:'',
+        fullName3:'',
     };
   },
-  methods: {
-    changeEmail() {
-      // this.emailValue이 이메일 형식에 맞는지 체크
-      const emailExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; //문자열 + @ + 문자열 + . + 문자열
-      if (this.emailValue.length == 0) {
-        this.msgEmail = "이메일을 입력하세요";
-      } else if (emailExp.test(this.emailValue) == true) {
-        this.msgEmail = "정상적인 이메일 주소입니다.";
-      } else {
-        this.msgEmail = "이메일 주소가 올바르지 않습니다.";
-      }
+  watch:{
+    familyName3(){
+        this.fullName3 = this.familyName3+ ' - ' + this.name3;
     },
-
-    changePwd1() {
-      const pwdRegex = /[@#]/;
-      if (this.pwdValue1 === "") {
-        this.errPwd = "비밀번호를 입력하세요";
-      } else if (pwdRegex.test(this.pwdValue1)) {
-        this.errPwd = "비밀번호에 '@' 또는 '#' 문자는 사용할 수 없습니다.";
-      } else if (this.pwdValue2 === "") {
-        this.errPwd = "비밀번호확인을 입력하세요";
-      } else if (this.pwdValue1.length < 8 || this.pwdValue2.length < 8) {
-        this.errPwd = "비밀번호는 최소 8자리수 이상입니다.";
-      } else if (this.pwdValue1 === this.pwdValue2) {
-        this.errPwd = "비밀번호가 유효합니다.";
-      } else {
-        this.errPwd = "비밀번호가 일치하지 않습니다.";
-      }
-    },
-    changePwd2() {
-      this.changePwd1();
-    },
-
-    changeCity() {
-      switch (this.city) {
-        case "서울":
-          this.message = "안녕하세요 반갑습니다.";
-          break;
-        case "부산":
-          this.message = "안녕하십니꺼 반갑스니더.";
-          break;
-        case "대전":
-          this.message = "안녕하셔유 반가워유.";
-          break;
-        case "수원":
-          this.message = "안녕하세요 반갑습니다.";
-          break;
-        case "광주":
-          this.message = "안녕하시오 반갑소잉.";
-          break;
-        default:
-          this.message = "안녕하세요 반갑습니다.";
-          break;
-      }
-    },
+    name3(){
+        this.fullName3 = this.familyName3+ ' - ' + this.name3;
+    }
   },
+  computed:{
+    makeFullName2(){
+        return this.familyName2+ ' - ' + this.name2;
+    }
+  },
+  methods: {
+    makeFullName1(){
+        this.fullName1 = this.familyName1+ ' - ' + this.name1;
+    }
+  }
 };
 </script>
-<style scoped></style>
+<style scoped>
+</style>
